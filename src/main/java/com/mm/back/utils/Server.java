7b3a71f4380f3057fcc5152ef6ca58b97e5e2cda
chenyanlong <574146905@qq.com>
2017-03-28 02:37:43 +0800

    1111

--- src/main/java/com/mm/back/utils/Server.java
+++ src/main/java/com/mm/back/utils/Server.java
@@ -1,9 +1,6 @@
 package com.mm.back.utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,31 +12,64 @@ import java.net.Socket;
  */
 public class Server {
 
+    public static final int PORT = 12345;//监听的端口号
+
     public static void main(String[] args) {
+        System.out.println("服务器启动...\n");
+        Server server = new Server();
+        server.init();
+    }
+
+    public void init() {
         try {
-            //创建绑定到特定端口的服务器套接字
-            ServerSocket serverSocket = new ServerSocket(9999);
-            System.out.println("***服务器即将启动，等待客户端链接***");
-            //侦听并接受到此套接字的连接
-            Socket socket = serverSocket.accept();
-            //获取输入流
-            InputStream is = socket.getInputStream();
-            //将字节输入流转为字符输入流
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String str = null;
-            while ((str = br.readLine()) != null) {
-                System.out.println("服务器说：客户端发送了：" + str);
-                br.readLine();
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            while (true) {
+                // 一旦有堵塞, 则表示服务器与客户端获得了连接
+                Socket client = serverSocket.accept();
+                // 处理这次连接
+                new HandlerThread(client);
             }
-            //此套接字的输入流置于"流的末尾"
-            socket.shutdownInput();
-            br.close();
-            isr.close();
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("服务器异常: " + e.getMessage());
+        }
+    }
+
+    private class HandlerThread implements Runnable {
+        private Socket socket;
+        public HandlerThread(Socket client) {
+            socket = client;
+            new Thread(this).start();
         }
 
+        public void run() {
+            try {
+                // 读取客户端数据
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
+                // 处理客户端数据
+                System.out.println("客户端发过来的内容:" + clientInputStr);
+
+                // 向客户端回复信息
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                System.out.print("请输入:\t");
+                // 发送键盘输入的一行
+                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                out.writeUTF(s);
+
+                out.close();
+                input.close();
+            } catch (Exception e) {
+                System.out.println("服务器 run 异常: " + e.getMessage());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (Exception e) {
+                        socket = null;
+                        System.out.println("服务端 finally 异常:" + e.getMessage());
+                    }
+                }
+            }
+        }
     }
 }
